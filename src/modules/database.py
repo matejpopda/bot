4e5af70037758552dde7771a072b95f6522d7abc
matejpopda <20053580+matejpopda@users.noreply.github.com>
2025-12-12 22:51:37 +0100

    fixed a bug with database

--- src/modules/database.py
+++ src/modules/database.py
@@ -1,11 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base, DeclarativeMeta
-
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///sqlite-database.db"
 engine = create_async_engine(DATABASE_URL)
 
-AsyncSessionLocal = sessionmaker(
+AsyncSessionLocal = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
 
