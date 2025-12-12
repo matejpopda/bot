ed6ce5a4f875823606369311ec1461bf6ad4a21d
matejpopda <20053580+matejpopda@users.noreply.github.com>
2025-12-12 23:28:31 +0100

    fixxed a bug where some connection games wouldnt get logged

--- src/modules/daily_games/parsers/connections.py
+++ src/modules/daily_games/parsers/connections.py
@@ -5,6 +5,8 @@ from .. import utils
 from ..daily_games import register_parser
 from ..daily_games import add_game_info
 
+import string
+
 CONNECTIONS_ORIGIN_DATE = datetime.date(day=11, month=6, year=2023)
 game_name = "Connections"
 
@@ -24,13 +26,21 @@ add_game_info(game_name, game_info)
 pattern = re.compile(
     r"^(?P<game>Connections)\s*\nPuzzle\s*#(?P<number>\d+)", re.MULTILINE
 )
-# Regex for same-colored lines (all emojis identical)
-same_color_pattern = re.compile(r"^([🟦🟩🟨🟪🟧🟥🟫])\1*$", re.MULTILINE)
-# Regex for multicolored lines (at least 2 different emojis)
-multicolor_pattern = re.compile(
-    r"^(?!([🟦🟩🟨🟪🟧🟥🟫])\1*$)[🟦🟩🟨🟪🟧🟥🟫]+$", re.MULTILINE
-)
 
+def count_mixed_lines(lines):
+    count = 0
+        
+    for line in lines:
+        if not line:
+            continue
+        if len(line) != 4:
+            continue
+
+        if any(ch not in string.ascii_lowercase for ch in line):
+            continue
+        
+        if len(set(line)) > 1: # converting to set to find out if its unique
+            count += 1
 
 @register_parser(game_name, r"^Connections")
 def connections_parser(message: discord.Message):
@@ -42,15 +52,39 @@ def connections_parser(message: discord.Message):
         return
     result = data.groupdict()
 
-    grid_text = "\n".join(text.splitlines()[2:])
 
-    same_color_count = len(same_color_pattern.findall(grid_text))
-    multicolor_count = len(multicolor_pattern.findall(grid_text))
 
-    score = multicolor_count
+    score = count_mixed_lines(text.splitlines()[2:])
 
     game_number = int(result["number"])
 
     date = utils.date_after_days_passed(CONNECTIONS_ORIGIN_DATE, game_number)
 
-    return float(score), date, int(game_number)
+    return score, date, game_number
+
+
+
+
+
+pattern_infinite = re.compile(
+    r"^(?P<game>Connections)\s*#(?P<number>\d+)", re.MULTILINE
+)
+
+
+@register_parser(game_name, r"^Connections ")
+def connections_parser_2(message: discord.Message):
+
+    text = message.content
+
+    data = pattern_infinite.search(text)
+    if data is None:
+        return
+    result = data.groupdict()
+
+
+    score = count_mixed_lines(text.splitlines()[1:])
+    game_number = int(result["number"])
+
+    date = utils.date_after_days_passed(CONNECTIONS_ORIGIN_DATE, game_number)
+
+    return score, date, game_number
